895085f324b1bec5388c3b361b87faed38b5cb17
Mark Deazley <mdeazley@gmail.com>
2019-11-19 22:02:36 -0700

    Using public-api import.

--- projects/alphapulsar-babylonjs-itest-app/src/app/app.module.ts
+++ projects/alphapulsar-babylonjs-itest-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AlphapulsarBabylonjsLibModule} from '../../../alphapulsar-babylonjs-lib/src/lib/alphapulsar-babylonjs-lib.module';
+import {AlphapulsarBabylonjsLibModule} from '../../../alphapulsar-babylonjs-lib/src/public-api';
 
 @NgModule({
   declarations: [
