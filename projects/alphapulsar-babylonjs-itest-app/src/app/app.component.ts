75780446d3dd57bef3092574e5d18c7b446dab33
Mark Deazley <mdeazley@gmail.com>
2019-11-20 12:31:39 -0700

    Version to 1.0.1.

--- projects/alphapulsar-babylonjs-itest-app/src/app/app.component.ts
+++ projects/alphapulsar-babylonjs-itest-app/src/app/app.component.ts
@@ -28,7 +28,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AlphaPulsar BabylonJS Integration Test';
-  version = '1.0.0';
+  version = '1.0.1'
   author = 'mdeazley@gmail.com';
   description = 'This application is the integration test for the babylonjs-canvas component.';
 }
